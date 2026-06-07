eaf5e7a836c5c0d490c7ecd48738b2c3155ed11a
Abrar <abrar.rahman.shafin@g.bracu.ac.bd>
2026-06-07 19:57:46 +0600

    Show agent cards

--- index.js
+++ index.js
@@ -7,14 +7,28 @@ const API_URL = "https://valorant-api.com/v1/"
 
 app.use(express.static("public"));
 
-app.get("/", async (req, res) => {
-    const result = await axios.get(API_URL + "agents");
+app.get("/", (req, res) => {
     res.render("index.ejs", {
-        // content: JSON.stringify(result.data)
         title: "Valorant Hub",
         description:
             "Your ultimate Valorant companion — explore agents, maps, weapons, and game modes. Built for players who want faster info and smarter gameplay."
     })
+})
+
+app.get("/agents", async (req, res) => {
+    try {
+        const result = await axios.get(API_URL + "agents");
+        const agents = result.data.data.filter(
+            (agent) => agent.isPlayableCharacter === true
+        );
+        res.render("agents.ejs", {
+            agents
+        })
+    } catch (error) {
+        console.log(error.message);
+        res.send("Error loading agents");
+    }
+
 })
 
 app.listen(port, () => {
